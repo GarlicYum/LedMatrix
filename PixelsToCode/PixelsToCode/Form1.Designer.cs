42a8a70bb88b57a7a8da4a0f7ec6377cd651c517
StaffanPersson <staffan.persson@hotmail.com>
2021-07-22 21:56:35 +0200

    Added projects

---
+++ PixelsToCode/PixelsToCode/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace PixelsToCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(340, 650);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(104, 38);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Red;
+            this.btnColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor.Location = new System.Drawing.Point(42, 651);
+            this.btnColor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(50, 38);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(226, 650);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(104, 38);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 715);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnExport);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Pixel Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ PixelsToCode/PixelsToCode/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PixelsToCode
+{
+    public partial class Form1 : Form
+    {
+        List<Button> cells = new List<Button>();
+        int gridDim = 24;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            int offset = 25;
+            int btnDim = 25;
+
+            for( int y = 0; y < gridDim; y++ )
+            {
+                for( int x = 0; x < gridDim; x++ )
+                {
+                    Button btn = new Button();
+                    btn.Width = btnDim;
+                    btn.Height = btnDim;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.Location = new Point(offset + x * btnDim, offset + y * btnDim);
+                    btn.BackColor = Color.Black;
+                    btn.ForeColor = Color.DarkGray;
+                    btn.Click += ColorCellClick;
+                    Controls.Add( btn );
+                    cells.Add( btn );
+                }
+            }
+        }
+
+        private void btnExport_Click( object sender, EventArgs e )
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.DefaultExt = "txt";
+
+            if( dialog.ShowDialog() == DialogResult.OK )
+            {
+                Save(dialog.FileName);
+            }
+        }
+
+        private void btnColor_Click( object sender, EventArgs e )
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.Color = btnColor.BackColor;
+
+            if( dialog.ShowDialog() == DialogResult.OK )
+                btnColor.BackColor = dialog.Color;
+        }
+
+        private void Save(string fileName)
+        {
+            string text = "const long Frame[] PROGMEM =\n" +
+                "{\n";
+
+            for( int i = 0; i < gridDim; i++ )
+            {
+                for( int j = 0; j < gridDim; j++ )
+                {
+                    int index = i * gridDim + j;
+                    Color c = cells[index].BackColor;
+
+                    text += "0x" + c.R.ToString( "X2" ) + 
+                        c.G.ToString( "X2" ) + 
+                        c.B.ToString( "X2" );
+
+                    if( index < (cells.Count - 1) )
+                    {
+                        text += ",";
+                    }
+                }
+                
+                text += "\n";
+            }
+
+            text += "};";
+
+            using( var writer = new StreamWriter( fileName ) )
+            {
+                writer.Write( text );
+            }
+        }
+
+        private void ColorCellClick( object sender, EventArgs e )
+        {
+            ( sender as Button ).BackColor = btnColor.BackColor;
+        }
+
+        private void btnLoad_Click( object sender, EventArgs e )
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if( dialog.ShowDialog() == DialogResult.OK )
+            {
+                LoadFile( dialog.FileName );
+            }
+        }
+
+        private void LoadFile(string fileName)
+        {
+            var rgbText = File.ReadAllText(fileName).Split('{')[1].Split('}')[0].Split(',');
+
+            for( int i = 0; i < cells.Count; i++ )
+            {
+                Color c = ColorTranslator.FromHtml( rgbText[i].Replace( "0x", "#" ) );
+                cells[i].BackColor = c;
+            }
+        }
+    }
+}

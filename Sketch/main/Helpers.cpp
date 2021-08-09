9a6a3c50eaf5365cbe4f0d0302b34b675e2f2066
StaffanPersson <staffan.persson@hotmail.com>
2021-08-09 07:19:18 +0200

    Fixed bug in convertIndex

--- Sketch/main/Helpers.cpp
+++ Sketch/main/Helpers.cpp
@@ -3,5 +3,5 @@
 
 int Helpers::convertIndex(int i)
 {
-  return i % UNIT_DIM + ( i / UNIT_DIM ) * LEDS_PER_UNIT - ( i / ROW_SIZE ) * (UNITS_PER_ROW * LEDS_PER_UNIT) + UNIT_DIM * (i / ROW_SIZE);
+  return i % UNIT_DIM + ( i / UNIT_DIM ) * LEDS_PER_UNIT - ( i / ROW_SIZE ) * ( UNITS_PER_ROW * LEDS_PER_UNIT ) + ((UNIT_DIM * ( i / ROW_SIZE )) % LEDS_PER_UNIT) + (i / (LEDS_PER_UNIT * UNITS_PER_ROW)) * (LEDS_PER_UNIT * UNITS_PER_ROW);
 }

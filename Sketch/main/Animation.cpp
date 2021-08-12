79d912817978423342cdedf53c1d051004150963
StaffanPersson <staffan.persson@hotmail.com>
2021-08-12 23:35:18 +0200

    Make color brightness adjustable gifdsf

--- Sketch/main/Animation.cpp
+++ Sketch/main/Animation.cpp
@@ -2,6 +2,8 @@
 #include "Helpers.h"
 #include "Constants.h"
 
+#define COLOR_BRIGHTNESS 1.0
+
 Animation::Animation()
 {}
 
@@ -22,14 +24,30 @@ void Animation::updateAnim(CRGB* leds)
   {
     for(int i = 0; i < NUM_LEDS; i++)
     {
-      leds[Helpers::convertIndex(i)] = pgm_read_dword(&(_frames[_frameIndices[_currentFrameCount] * NUM_LEDS + i]));
+      unsigned int color = pgm_read_dword(&(_frames[_frameIndices[_currentFrameCount] * NUM_LEDS + i]));
+      byte r = (byte)((double)((color & 0xFF000000) >> 24) * COLOR_BRIGHTNESS);
+      byte g = (byte)((double)((color & 0xFF000000) >> 16) * COLOR_BRIGHTNESS);
+      byte b = (byte)((double)((color & 0xFF000000) >> 8) * COLOR_BRIGHTNESS);
+      byte a = (byte)((double)((color & 0xFF000000) >> 0) * COLOR_BRIGHTNESS);
+
+      color = r << 24 + g << 16 + b << 8 + a;
+
+      leds[Helpers::convertIndex(i)] = color;
     }
   }
   else
   {
     for(int i = 0; i < NUM_LEDS; i++)
     {
-      leds[Helpers::convertIndex(i)] = pgm_read_dword(&(_frames[_currentFrameCount * NUM_LEDS + i]));
+      unsigned int color = pgm_read_dword(&(_frames[_currentFrameCount * NUM_LEDS + i]));
+      byte r = (byte)((double)((color & 0xFF000000) >> 24) * COLOR_BRIGHTNESS);
+      byte g = (byte)((double)((color & 0xFF000000) >> 16) * COLOR_BRIGHTNESS);
+      byte b = (byte)((double)((color & 0xFF000000) >> 8) * COLOR_BRIGHTNESS);
+      byte a = (byte)((double)((color & 0xFF000000) >> 0) * COLOR_BRIGHTNESS);
+
+      color = r << 24 + g << 16 + b << 8 + a;
+      
+      leds[Helpers::convertIndex(i)] = color;
     }
   }
    
